88779248ea4afbc570b81c5b171dcac7524e517d
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-09 13:05:05 +0200

    Install "laravel/framework"

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'Hello, world! ' . PHP_VERSION;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+require __DIR__ . '/vendor/autoload.php';
+
+Application::configure(__DIR__ . DIRECTORY_SEPARATOR . 'laravel')
+    ->withRouting(function (): void {
+        Route::get('/', fn() => view('index', ['phpVersion' => \PHP_VERSION]));
+    })
+    ->create()
+    ->handleRequest(Request::capture());
