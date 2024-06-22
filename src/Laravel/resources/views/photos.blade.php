b4c5fb961af44665817703a735a2281a8b762c45
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-06-22 17:34:41 +0200

    Lazy load images

--- src/Laravel/resources/views/photos.blade.php
+++ src/Laravel/resources/views/photos.blade.php
@@ -9,14 +9,56 @@
         max-height: 150px;
         box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
     }
+
+    img.photo[data-src] {
+        visibility: hidden;
+    }
 </style>
 <div class="images">
- 
-  @forelse($images as $image)
-    <img src="{{$image}}" alt="{{$image}}" class="photo"/>
+  @forelse($images as $index => $image)
+    @if ($index < 20)
+      <img src="{{$image}}" alt="{{$image}}" class="photo"/>
+    @else
+      <img data-src="{{$image}}" data-id="{{$index}}" alt="{{$image}}" class="photo"/>
+    @endif
   @empty
     <h4 style="text-align:center; opacity:0.5; margin-top:25vh;">
       Wrzuć pierwsze zdjęcie!
     </h4>
   @endforelse
 </div>
+
+<script>
+  let openForScroll = false;
+
+  function load(image, callback) {
+    if (image.getBoundingClientRect().top < window.innerHeight) {
+      images.shift();
+      image.onload = image.onerror = callback;
+      image.src = image.dataset.src;
+      delete image.dataset.src;
+    } else {
+      openForScroll = true;
+    }
+  }
+
+  function loadNextImage(images) {
+    if (images.length === 0) {
+      return;
+    }
+    requestIdleCallback(() => {
+      load(images[0], () => loadNextImage(images));
+    });
+  }
+
+  const images = Array.from(document.querySelectorAll(".images img[data-src]"));
+  window.addEventListener("DOMContentLoaded", function () {
+    loadNextImage(images);
+    window.addEventListener("scroll", () => {
+      if (openForScroll) {
+        openForScroll = false;
+        loadNextImage(images);
+      }
+    });
+  });
+</script>
