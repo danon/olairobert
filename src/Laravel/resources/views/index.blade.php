5ed9323b7c2c275798991c5a0b0a10f749a2f959
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-09-08 23:43:26 +0200

    Update html <title>

--- src/Laravel/resources/views/index.blade.php
+++ src/Laravel/resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <title>Wesele Oli i Roberta</title>
+  <title>Wesele Aleksandry i Roberta</title>
   <meta name="viewport" content="width=device-width,initial-scale=1"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <style>
